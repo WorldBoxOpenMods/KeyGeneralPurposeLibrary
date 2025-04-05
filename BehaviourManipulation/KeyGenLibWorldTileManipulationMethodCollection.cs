6f4d8ae4310b7f97b0b9abe55641528f19d3e42a
key44 <modding-contributions@keymasterer.uk>
2025-04-05 16:30:50 +0200

    minor capitalisation fixes

--- BehaviourManipulation/KeyGenLibWorldTileManipulationMethodCollection.cs
+++ BehaviourManipulation/KeyGenLibWorldTileManipulationMethodCollection.cs
@@ -4,18 +4,18 @@ using System.Linq;
 namespace KeyGeneralPurposeLibrary.BehaviourManipulation {
   public class KeyGenLibWorldTileManipulationMethodCollection : KLibComponent {
     public void ChangeEveryWorldTile(Action<WorldTile, string> action) {
-      foreach (WorldTile tile in World.world.tilesList.Where(tile => tile != null)) {
+      foreach (WorldTile tile in World.world.tiles_list.Where(tile => tile != null)) {
         action(tile, null);
       }
     }
 
     public void ChangeSpecificWorldTiles(Action<WorldTile, string> action, Func<WorldTile, bool> requirement) {
-      foreach (WorldTile tile in World.world.tilesList.Where(tile => tile != null && requirement(tile))) {
+      foreach (WorldTile tile in World.world.tiles_list.Where(tile => tile != null && requirement(tile))) {
         action(tile, null);
       }
     }
     public void ChangeSpecificWorldTiles(Action<WorldTile> action, Func<WorldTile, bool> requirement) {
-      foreach (WorldTile tile in World.world.tilesList.Where(tile => tile != null && requirement(tile))) {
+      foreach (WorldTile tile in World.world.tiles_list.Where(tile => tile != null && requirement(tile))) {
         action(tile);
       }
     }
